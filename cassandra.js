335fad28d436d8a658431ddc7f5e34dcb96f7f8a
James CC Yu <cyu021@gmail.com>
2016-03-16 16:44:01 +0800

    comment out debug log
    comment out debug log

--- cassandra.js
+++ cassandra.js
@@ -3,17 +3,17 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
-			console.log('msg = ' + JSON.stringify(msg));
+			//console.log('msg = ' + JSON.stringify(msg));
 			var cassandra = msg.cassandra;
 
 			var contactPoints = config.contactPoints;
 			var keyspace = config.keyspace;
-			console.log('contactPoints = ' + contactPoints);
-			console.log('keyspace = ' + keyspace);
+			//console.log('contactPoints = ' + contactPoints);
+			//console.log('keyspace = ' + keyspace);
 
 			var payload = msg.payload;
 			var cql = payload.cql;
-			console.log('cql = ' + cql);
+			//console.log('cql = ' + cql);
 			
 			var client = new cassandra.Client({contactPoints: [contactPoints], keyspace: keyspace});
 			var query = cql;
@@ -21,12 +21,12 @@ module.exports = function(RED) {
 			var params = [];
 			var i = 0;
 			for(i = 0; i < payload.params.length; i += 1) {
-				console.log('v = ' + payload.params[i].v);
+				//console.log('v = ' + payload.params[i].v);
 				params.push(payload.params[i].v);
 			}
 			client.execute(query, params, { prepare: true}, function (err, result) {
-				console.log('result = ' + JSON.stringify(result));
-				console.log('err = ' + JSON.stringify(err));
+				//console.log('result = ' + JSON.stringify(result));
+				//console.log('err = ' + JSON.stringify(err));
 				msg.result = JSON.stringify(result);
 				msg.err = JSON.stringify(err);
 				node.send(msg);
